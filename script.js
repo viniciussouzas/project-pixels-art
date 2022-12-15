836efcec70b9259959ae28aa1ad51beb7f84261f
Vinícius Souza <tradutorvinicius@gmail.com>
2022-12-15 18:01:40 -0300

    Refatora código novamente, optei por fazer a criação de elementos no html ao invés de dinamicamente no JavaScript, e resolve requisito 4

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
+window.onload = () => {
+  addColorToPalette();
+  //addRandomColor();
+}
+
+//Captura Elementos:
 const body = document.getElementById('body');
 const paletteSpaces = document.getElementsByClassName('color');
 const colorsOfPalette = ['black', 'red', 'green', 'blue'];
+const buttonRandomColor = document.getElementById('button-random-color');
 
-//Requisito 1 - Feito direto no HTML
+//Resolve Requisitos:
 
-//Requisito 2
+// - Requisito 1 - Feito direto no HTML
 
-const addPallete = () => {
-  const createPallete = document.createElement('div');
-  createPallete.id = 'color-palette';
+// - Requisito 2 e 3 - Parte feita no HTML de criação de Elementos
 
-  for (let index = 1; index <= 4; index += 1) {
-    const createPalleteSpaces = document.createElement('div');
-    createPalleteSpaces.className = 'color';
-
-    createPallete.appendChild(createPalleteSpaces);
-  }
-  body.appendChild(createPallete);
-}
-
-const addColorToPalete = () => {
+const addColorToPalette = () => {
   for (let index = 1; index < paletteSpaces.length; index += 1) {
     for (let index = 0; index < colorsOfPalette.length; index += 1) {
       paletteSpaces[index].style.backgroundColor = colorsOfPalette[index];
@@ -27,9 +23,16 @@ const addColorToPalete = () => {
   }
 }
 
+//Requisito 4
 
+const addRandomColor = () => {
+  for (let index = 1; index < paletteSpaces.length; index += 1) {
+    let r = Math.random() * 255;
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
 
-window.onload = () => {
-  addPallete();
-  addColorToPalete();
+    paletteSpaces[index].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  }
 }
+
+buttonRandomColor.addEventListener('click', addRandomColor);
